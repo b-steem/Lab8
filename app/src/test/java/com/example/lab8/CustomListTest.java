99e56686dc53464caa7740540a7277435f5df6fc
Bennet Steem <bennet.steem@gmail.com>
2024-03-13 11:31:50 -0600

    Added CustomListTest.java to test addCity method

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
+    /**
+     * create a mocklist for my citylist
+     * @return list - the new customlist
+     */
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
+    /**
+     * checks that a new city is added to mocklist by testing the addCIty function
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 }
